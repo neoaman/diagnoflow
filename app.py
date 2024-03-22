12187ba1383d4a064f610856f32e099aacb8939e
neoaman <amanthe001@gmail.com>
2024-03-22 09:14:54 +0000

    feat: Added streamlit bot

--- app.py
+++ app.py
@@ -1,197 +1,140 @@
-import dash_mantine_components as dmc
-from dash import Dash, html, Input, Output, State, dcc
-import json
-from urllib.request import urlretrieve
-import dash_auth
-from dash_iconify import DashIconify
-from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import OllamaEmbeddings
-from langchain.memory import ConversationBufferMemory
-from operator import itemgetter
-from langchain_core.messages import AIMessage, HumanMessage, get_buffer_string
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.prompts import format_document
-
-from langchain_core.runnables import RunnablePassthrough, RunnableLambda
-from langchain_core.output_parsers import StrOutputParser
-from langchain import hub
 from langchain.llms import Ollama
-from langchain.prompts.prompt import PromptTemplate
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+import streamlit as st
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
+from langchain.memory import StreamlitChatMessageHistory
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+import requests
 
-# BASE_URL = "https://d1agn0fl0w.loca.lt/"
-# BASE_URL = "https://diagnoflow.loca.lt/"
-BASE_URL = "http://0.0.0.0:11434/"
-vector_embedding = "gemma:2b"
-llm_model = "gemma"
+from model import get_patient_yaml
 
+st.set_page_config(layout="wide")
 
-# Download the Vectorstore if unavailable
-try:
-    db = FAISS.load_local("assets/data/medical_index",embeddings=OllamaEmbeddings(model=vector_embedding,base_url=BASE_URL),allow_dangerous_deserialization=True)
-except Exception as e:
-    with open("assets/data/config.json",'r') as cj:
-        vector_config = json.load(cj)
-    urlretrieve(vector_config["index_faiss_url"],"assets/data/medical_index/index.faiss")
-    urlretrieve(vector_config["index_pkl_url"],"assets/data/medical_index/index.pkl")
-    db = FAISS.load_local("assets/data/medical_index",embeddings=OllamaEmbeddings(model=vector_embedding,base_url=BASE_URL),allow_dangerous_deserialization=True)
-
-
-# Create the RAG with langchain
-prompt = hub.pull("rlm/rag-prompt")
-retriever = db.as_retriever()
-llm = Ollama(model=llm_model,base_url=BASE_URL,callback_manager = CallbackManager([StreamingStdOutCallbackHandler]))
-
-rag_chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
+if "patient_name" not in st.session_state: 
+    st.session_state["patient_name"] = ""
+if "patient_info" not in st.session_state: 
+    st.session_state["patient_info"] = ""
 
 
+with st.sidebar:
+    base_url=st.text_input("Enter the ollama base url","http://0.0.0.0:11434")
+    try:
+        model_options = [i["name"] for i in requests.get(f"{base_url}/api/tags").json()["models"]]
+    except:
+        st.toast(":red[Invalid ollama base url]")
+        model_options = [""]
+    selected_model = st.selectbox("Select llm model",options=model_options)
+    context = st.text_area("Provided context","""
+    blood_pressure:
+    diastolic: 90 mmHg
+    systolic: 135 mmHg
+    blood_type: O-
+    date_of_birth: '1970-12-30'
+    gender: Male
+    heart_rate: 72 bpm
+    height: 178 cm
+    patient_name: Liam Wilson
+    reports:
+    - date: '2024-03-17'
+    description: High Blood Pressure
+    findings: Hypertension detected, need for lifestyle modifications
+    recommendations:
+    - Begin medication as prescribed
+    - Reduce salt intake
+    - Increase physical activity
+    temperature: "36.9 °C"
+    weight: 85 kg
+    """)
 
-# HACK New approach
+ask_name = st.empty()
+with ask_name:
+    patient_name = st.text_input("Username")
+    st.session_state["patient_name"] = patient_name
+    st.session_state["patient_info"] = get_patient_yaml(st.session_state["patient_name"])
 
+if st.session_state["patient_name"]=="":
+    st.stop()
+else:
+    with ask_name:
+        st.empty()
 
+initial_instruction = f"""
+You are a personalized assistance for patients, based on provided context below in json format, please answer the questions of patient.
+Please write name of the parient while greeting and interacting.
 
+Below is one example for you with context and how to handel the questions.
 
-DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
+Context:
+---------------------------
+blood_pressure:
+  diastolic: 90 mmHg
+  systolic: 135 mmHg
+blood_type: O-
+date_of_birth: '1970-12-30'
+gender: Male
+heart_rate: 72 bpm
+height: 178 cm
+patient_name: Liam Wilson
+reports:
+- date: '2024-03-17'
+  description: High Blood Pressure
+  findings: Hypertension detected, need for lifestyle modifications
+  recommendations:
+  - Begin medication as prescribed
+  - Reduce salt intake
+  - Increase physical activity
+temperature: "36.9 \xB0C"
+weight: 85 kg
+---------------------------
 
+Question: Hi
+Hello Liam How can I help you ?
 
-memory = ConversationBufferMemory(
-    return_messages=True, output_key="answer", input_key="question"
-)
-loaded_memory = RunnablePassthrough.assign(
-    chat_history=RunnableLambda(memory.load_memory_variables) | itemgetter("history"),
-)
-_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
-Chat History:
-{chat_history}
-Follow Up Input: {question}
-Standalone question:"""
-CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+Question: how are my reports?.
+Liam, based on your report doctor identified that you have high blood pressure.
 
-standalone_question = {
-    "standalone_question": {
-        "question": lambda x: x["question"],
-        "chat_history": lambda x: get_buffer_string(x["chat_history"]),
-    }
-    | CONDENSE_QUESTION_PROMPT
-    | llm
-    | StrOutputParser(),
-}
-# Now we retrieve the documents
-retrieved_documents = {
-    "docs": itemgetter("standalone_question") | retriever,
-    "question": lambda x: x["standalone_question"],
-}
-# Now we construct the inputs for the final prompt
-def _combine_documents(
-    docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"
-):
-    doc_strings = [format_document(doc, document_prompt) for doc in docs]
-    return document_separator.join(doc_strings)
-final_inputs = {
-    "context": lambda x: _combine_documents(x["docs"]),
-    "question": itemgetter("question"),
-}
-# And finally, we do the part that returns the answers
-template = """Answer the question based only on the following context:
-{context}
+Question: What are the recommendations?
+Liam, You need to begin the prescribed medication by doctor, and do some physical activities, please reduce your salt intake.
 
-Question: {question}
+=============================================================================================================
+Now its YOUR TURN to interact with the patient REMEMBER, answer should be short, don't act smart and answer should be specific, always say the patient name in response.
+
+Context:
+---------------------------
+{st.session_state["patient_info"]}
+---------------------------
 """
-ANSWER_PROMPT = ChatPromptTemplate.from_template(template)
-answer = {
-    "answer": final_inputs | ANSWER_PROMPT | llm,
-    "docs": itemgetter("docs"),
-}
-# And now we put it all together!
-final_chain = loaded_memory | standalone_question | retrieved_documents | answer
 
+# print(requests.get(f"{base_url}/api/tags").json())
 
+template = initial_instruction+"""
 
+{history}
+Human: {human_input}
+AI: """
+prompt_template = PromptTemplate(input_variables=["history", "human_input"], template=template)
 
+msgs = StreamlitChatMessageHistory(key="follow_up_bot_key")
+memory = ConversationBufferMemory(memory_key="history", chat_memory=msgs)
+if len(msgs.messages) == 0:
+    msgs.add_ai_message(f"Hi {st.session_state['patient_name']}, How can I help you?")
 
+llm = Ollama(model = selected_model, base_url=base_url,callback_manager = CallbackManager([StreamingStdOutCallbackHandler]))
+llm_chain = LLMChain(llm=llm, prompt=prompt_template, memory=memory)
+        
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
+for msg in msgs.messages:
+    st.chat_message(msg.type).markdown(msg.content)
 
-app = Dash(__name__)
-server = app.server # needed for gunicorn 
+if prompt := st.chat_input("Say something: "):
+    st.chat_message("user").markdown(prompt)
 
-
-app.layout = html.Div([
-    dcc.Store(id="store_chat_history",data=[("bot","Hello, I am DiagnoFlow assistance, How can I help you ?")]),
-    dmc.Title("DiagnoFlow"),
-    dmc.Alert(
-        "Hi from Neo (creator of DiagnoFlow). We will like to serve you!",
-        title="Welcome to DiagnoFlow",
-        color="violet",
-    ),
-    dmc.Affix(
-        dmc.Button("DiagnoFlow Assist",id="btn_chatbox",n_clicks=1,color="violet"), position={"bottom": 20, "right": 20}
-    ),
-
-    dmc.Affix(
-        dmc.Stack(
-            [
-                dmc.Paper(id="conversationbox"),
-                dcc.Input(placeholder="Write your query and hit enter",id="input_query",debounce=True,style={'margin':'10px'})
-            ],
-            className="glass chatbox",
-            align="stretch",justify="flex-end"
-            ),
-            position={"bottom": 60, "right": 20},
-            id="chatbox",
-    ),
-
-]
-)
-
-@app.callback(
-    Output("chatbox","style"),
-    Input("btn_chatbox","n_clicks")
-)
-def toggle_chatbox(btn):
-    return ({"display":"none"} if (btn % 2 == 0) else {"display":""})
-
-@app.callback(
-    Output("conversationbox","children"),
-    Input("store_chat_history","data"),
-)
-def display_conversation(data):
-
-    conversation = []
-    for user,message in data:
-        conversation.append(dmc.Paper(message,className=user))
-    return dmc.Stack(conversation,align="stretch",justify="flex-end")
-
-@app.callback(
-    Output("store_chat_history","data"),
-    Output("input_query","value"),
-    State("store_chat_history","data"),
-    Input("input_query","value"),
-    prevent_initial_call=True
-)
-def display_conversation(history,query):
-    if not query: return history
-    history.append(("human",query))
-    history.append(("bot",rag_chain.invoke(query)))
-
-    # result = final_chain.invoke({"question":query})
-    # print(result["answer"])
-    # history.append(("bot",result["answer"]))
-
-    # print("="*20)
-    # print(result)
-
-
-    return history,""
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
-
-# gunicorn main:server -b 0.0.0.0:8050 --reload
+    response = llm_chain.run(prompt)
+    with st.chat_message("assistant"):
+        st.markdown(response)
---
+++ model.py
@@ -0,0 +1,80 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+import json
+import yaml
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Create SQLite engine
+engine = create_engine('sqlite:///patient_database.db', echo=True)
+
+# Base class for declarative class definitions
+Base = declarative_base()
+
+# Define Patient ORM model
+class Patient(Base):
+    __tablename__ = 'patients'
+
+    id = Column(Integer, primary_key=True)
+    patient_name = Column(String)
+    date_of_birth = Column(String)
+    gender = Column(String)
+    blood_type = Column(String)
+    height = Column(String)
+    weight = Column(String)
+    systolic_bp = Column(String)
+    diastolic_bp = Column(String)
+    heart_rate = Column(String)
+    temperature = Column(String)
+    reports = relationship("Report", back_populates="patient")
+
+# Define Report ORM model
+class Report(Base):
+    __tablename__ = 'reports'
+
+    id = Column(Integer, primary_key=True)
+    patient_id = Column(Integer, ForeignKey('patients.id'))
+    date = Column(String)
+    description = Column(String)
+    findings = Column(String)
+    recommendations = Column(String)
+    patient = relationship("Patient", back_populates="reports")
+
+# Create tables in the database
+Base.metadata.create_all(engine)
+
+def get_patient_yaml(patient_name_to_search):
+    patient_data = session.query(Patient).filter_by(patient_name=patient_name_to_search).first()
+    if patient_data:
+        patient_dict = {
+            "patient_name": patient_data.patient_name,
+            "date_of_birth": patient_data.date_of_birth,
+            "gender": patient_data.gender,
+            "blood_type": patient_data.blood_type,
+            "height": patient_data.height,
+            "weight": patient_data.weight,
+            "blood_pressure": {
+                "systolic": patient_data.systolic_bp,
+                "diastolic": patient_data.diastolic_bp
+            },
+            "heart_rate": patient_data.heart_rate,
+            "temperature": patient_data.temperature,
+            "reports": []
+        }
+
+        # Add reports associated with the patient to the dictionary
+        for report in patient_data.reports:
+            report_dict = {
+                "date": report.date,
+                "description": report.description,
+                "findings": report.findings,
+                "recommendations": report.recommendations.split(", ")
+            }
+            patient_dict["reports"].append(report_dict)
+    else:
+        patient_dict = {}
+    patient_info_yaml = yaml.dump(patient_dict)
+    return patient_info_yaml
+
